67774c2c8bbd7c1e52daad11576b4c8fce5fc929
BaekChangKyeong <qorckdrud@naver.com>
2022-07-24 23:09:05 +0900

    connect designer personal page (kimgunwoo)

--- fastapi/main.py
+++ fastapi/main.py
@@ -23,6 +23,11 @@ def home(request: Request):
 def designer(request: Request):
     return templates.TemplateResponse("Designer.html", {"request":request})
 
+# get designer page
+@app.get(path="/designer/kimgunwoo")
+def designer(request: Request):
+    return templates.TemplateResponse("/designer/kimgunwoo.html", {"request":request})
+
 @app.get(path="/designer/{designer}")
 def designer(request: Request, designer):
     designer = "designer/" + designer
